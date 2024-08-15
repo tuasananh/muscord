f9938fb835b63f799305227921358fd7f686f505
kilkuwu <iamkilkuwu@gmail.com>
2024-08-16 02:19:09 +0700

    fix: Add DISCORD_GUILD_ID env var

--- src/types/Env.ts
+++ src/types/Env.ts
@@ -3,7 +3,7 @@ import { API } from '@discordjs/core/http-only';
 import { WorkerEntrypoint } from 'cloudflare:workers';
 
 interface Myself extends WorkerEntrypoint {
-  niceFunction: (arg: any) => Promise<any>;
+	niceFunction: (arg: any) => Promise<any>;
 }
 
 export default interface E extends Env {
@@ -11,7 +11,7 @@ export default interface E extends Env {
 		DISCORD_APPLICATION_ID: string;
 		DISCORD_PUBLIC_KEY: string;
 		DISCORD_TOKEN: string;
-    MYSELF: Myself
+		DISCORD_GUILD_ID: string;
 	};
 	Variables: {
 		api: API;
