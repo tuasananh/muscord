a0afdb48648334c7fe7ed457e5444921d3731e9f
kilkuwu <iamkilkuwu@gmail.com>
2025-09-24 13:21:22 +0700

    Remove test endpoint

--- src/index.ts
+++ src/index.ts
@@ -21,30 +21,6 @@ app.use(async (c, next) => {
 	await next();
 });
 
-app.get('/', async (c) => {
-	return c.text(`Hello ${c.env.DISCORD_APPLICATION_ID}`);
-});
-
-app.get('/test', async (c) => {
-	const filters = (await c.env.KV_STORE.get(
-		'filters',
-		'json'
-	)) as any;
-	console.log(filters);
-	return c.json(filters);
-});
-
-app.get('/test2', async (c) => {
-	await c.env.KV_STORE.put(
-		'filters',
-		JSON.stringify({
-			basic:
-				"-gay* -futa* -anthro*   -interspecies*  -animal* -feline* -comic* -monster* -winx* -size_difference -pokemon_(species) -alicorn* -king_of_the_hill  -cartoon* -male_only  -powerpuff_girls -wind_waker  -nickelodeon -rissma_(maewix1) -hazbin_hotel -furry -idw* -five_nights_at_freddy's -giratina -pseudoregalia -mass_effect -gacha  -tentacle* -ed_edd_n_eddy score:>=100",
-		})
-	);
-	return c.text('Done!');
-});
-
 app.post('/', verifyDiscordRequest, async (c) => {
 	const interaction: APIInteraction = await c.req.json();
 	console.log('Interaction type', interaction.type);
