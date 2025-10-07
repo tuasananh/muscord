aeb86ead074a54a1387ab76c1d744904cf45452d
kilkuwu <iamkilkuwu@gmail.com>
2025-10-07 19:36:24 +0700

    refactor: use cloudflare d1 for migrations

--- src/sql/mig0001.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS r34_presets;
-
-CREATE TABLE
-    r34_presets (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL UNIQUE CHECK (LENGTH (name) BETWEEN 1 AND 100),
-        discord_user_id TEXT NOT NULL,
-        content TEXT NOT NULL CHECK (LENGTH (content) BETWEEN 1 AND 4000)
-    );
